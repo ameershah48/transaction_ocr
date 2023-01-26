4df5485b6e9d82bfda2b637e790e84abe77d23bb
Ameer Shah <ameershah48@gmail.com>
2023-01-26 16:40:50 +0800

    Fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express')
 const fileUpload = require('express-fileupload');
 
 const app = express()
-const port = 3000
+const port = 3010
 
 app.use(fileUpload());
 app.use(express.json());
